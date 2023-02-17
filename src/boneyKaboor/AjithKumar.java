e291b99bfdde993b5aca627988dcf81badd1718e
vairamani2124 <vinimani2124@gmail.com>
2023-02-17 13:45:04 +0530

    first commit

---
+++ src/boneyKaboor/AjithKumar.java
@@ -0,0 +1,13 @@
+package boneyKaboor;
+
+public class AjithKumar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HERO OF THIS FILM");
+		
+
+	}
+
+}
