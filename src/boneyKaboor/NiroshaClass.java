39ec524ef507ab9514bfcd9f5470d358c1ef5d76
vairamani2124 <vinimani2124@gmail.com>
2023-02-17 14:52:12 +0530

    new tecn added

---
+++ src/boneyKaboor/NiroshaClass.java
@@ -0,0 +1,12 @@
+package boneyKaboor;
+
+public class NiroshaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("he is best cenimotpgrapher");
+
+	}
+
+}
